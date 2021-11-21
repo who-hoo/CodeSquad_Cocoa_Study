3c5fba12a8ba1ffdcac8a6fa63dec33bcbd9a547
donggi-lee-bit <af1505@naver.com>
2021-11-21 17:45:45 +0900

    Runnable interface를 이용한 스레드

--- content/thread/donggi/스레드/src/com/donggi/RunnableTest.java
+++ content/thread/donggi/스레드/src/com/donggi/RunnableTest.java
@@ -1,2 +1,20 @@
-package com.donggi;public class RunnableTest {
+package com.donggi;
+
+public class RunnableTest {
+
+  public static void main(String[] args) {
+    Runnable r = new runnableExam();
+    Thread runnableThread = new Thread(r);
+
+    runnableThread.start();
+  }
+}
+
+class runnableExam implements Runnable {
+
+  public void run() {
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Runnable 인터페이스를 사용한 스레드입니다 : " + Thread.currentThread().getName());
+    }
+  }
 }
